4b58e981432113bac22638919270c135b84cd137
wangtian-wang <57563364+wangtian-wang@users.noreply.github.com>
2021-09-03 08:24:07 +0800

    Update 实现一个多少秒之后执行的函数.js

--- code-skils/实现一个多少秒之后执行的函数.js
+++ code-skils/实现一个多少秒之后执行的函数.js
@@ -1,3 +1,19 @@
 /** 还有一种写法来着 微信收藏 */
 const wait = async (milliseconds) =>
   new Promise((resolve) => setTimeout(resolve, milliseconds));
+const sleep = delay => {
+     const start = new Date().getTime()
+     let  counter = 0
+     while (new Date().getTime() < start + delay) {
+         counter++;
+         continue
+     }
+     console.log(counter);
+    }
+    // sleep(3000)
+     
+ const getRate = ( rate = 0) => {
+     return '*****'.slice(5-rate,10-rate);
+ }
+
+const list = len => [...new Array(len).keys()]
